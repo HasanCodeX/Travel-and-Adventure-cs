b649361100d721b7f116e143ec330809d6e8957c
Your Name <your-email@example.com>
2024-12-10 02:11:02 +0600

    update

---
+++ src/mainLayOut/Root.jsx
@@ -0,0 +1,23 @@
+import { Outlet, useNavigation } from "react-router-dom";
+import Nav from "../shareComponents/Nav";
+import Footer from "../shareComponents/Footer";
+import 'react-tooltip/dist/react-tooltip.css';
+
+const Root = () => {
+    const navigation=useNavigation()
+    return (
+        <div>
+            <Nav></Nav>
+            <div className="min-h-[calc(100vh-288px)]">
+                {
+                    navigation.state === "submitting"
+                        ? <div className="flex justify-center items-center"><span className="loading loading-spinner loading-lg"></span></div> : <Outlet></Outlet>
+                }
+            </div>
+            <Footer></Footer>
+            
+        </div>
+    );
+};
+
+export default Root;
