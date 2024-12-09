95320c1396a779e54c83dc1d7950cef0f9d4c837
Your Name <your-email@example.com>
2024-12-10 02:06:38 +0600

    components

---
+++ src/shareComponents/Footer.jsx
@@ -0,0 +1,49 @@
+import { FaGoogle } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer p-10  bg-[#0a2233] text-white">
+                <aside>
+                    <img src="https://trendytraveldv.wpengine.com/wp-content/themes/trendytravel/images/logo.png" alt="" />
+                    <p>trendy travel Industries Ltd.<br />Providing reliable tech since 1992</p>
+                    <aside>
+                        <div className="flex justify-start space-x-7 py-5 text-3xl ">
+                            <FaGoogle></FaGoogle>
+                            <FaFacebook></FaFacebook>
+                            <FaTwitter></FaTwitter>
+                        </div>
+
+                        
+                        <p>Copyright © 2024 - All right reserved by trendy travel Industries Ltd</p>
+                    </aside>
+                </aside>
+                <nav>
+                    <h6 className="footer-title">Services</h6>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Company</h6>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Legal</h6>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </nav>
+                
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
